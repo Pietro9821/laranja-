0d83e0597b92093c7baf2881a3f32e04044466c7
Pietro.S <166620197+Pietro9821@users.noreply.github.com>
2024-04-16 11:54:55 -0300

    Add files via upload

---
+++ aula-6/index.js
@@ -0,0 +1,18 @@
+let nome 
+let idade 
+
+console.log(' tipo de variavel nome' ,typeof nome  )
+console.log(' tipo de variavel idade ', typeof idade )
+
+// let name = ( 'Pietro')
+// let idade = 17 
+
+ nome = prompt( 'qual o seu nome?')
+ idade = prompt( ' qual sua idade?')
+
+
+console.log( 'tipo de variavel nome', typeof nome)
+console.log('tipo de variavel idade', typeof idade )
+
+console.log( )
+
